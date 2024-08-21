16e48c9826e9886ebef9c5bdfd7434078b8232d1
azizah2101 <sitinurazizahhh2101@gmail.com>
2024-08-21 16:50:58 +0700

    Add files via upload
    Tugas Pertemuan ke 2

---
+++ pertemuan2tugaspamujib/Soalno2.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pertemuan2tugaspamujib;
+
+/**
+ *
+ * @author AZIZAH
+ */
+public class Soalno2 {
+    
+    public static String MakanandanminumanFavorit;
+    
+    public static void makanandanminuman(){
+    String MakananFavorit = "Ayam Geprek";
+         String MinumanFavorit = "Jus Jambu";
+         
+         System.out.println("Makanan Favorit Saya : Ayam geprek" +
+                 "Minuman Favoriit saya : Jus Jambu");
+         
+    }
+    
+    public static void main (String [] args) {
+        Soalno2 latihan = new Soalno2();
+    }
+     }
+     
+     
+      
+     
+     
+
