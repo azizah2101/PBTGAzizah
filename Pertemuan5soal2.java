9a3564659754bd23a2a42b3e169eef650814cbdc
azizah2101 <sitinurazizahhh2101@gmail.com>
2024-08-28 14:26:51 +0700

    Add files via upload
    pertemuan5 soal 2

---
+++ Pertemuan5soal2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan5soal2;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Pertemuan5soal2 {
+
+    public static void main(String[] args) {
+        
+        
+     double alas = 17; double tinggi= 11;
+     double luas = (alas*tinggi)/2;
+     
+     System.out.println("NilaiAlasdanTinggi:" + luas);
+    }
+}
