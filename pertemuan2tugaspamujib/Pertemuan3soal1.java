32bdee5bb6c28cae95e0c436dd670ee6410c6580
azizah2101 <sitinurazizahhh2101@gmail.com>
2024-08-28 12:41:27 +0700

    Add files via upload
    pertemuan3

---
+++ pertemuan2tugaspamujib/Pertemuan3soal1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan3soal1;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Pertemuan3soal1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int Var1 = 9;
+        float Var = 8.99f;
+        char Var3 = 'A';
+        boolean Var4 = false;
+        String Var5 = "Hello world";
+        
+    }
+}
